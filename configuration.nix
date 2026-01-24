c9048de394d52b2fd9b648383ce44c3433562a3b
Kristjan <krebeljk@gmail.com>
2026-01-24 17:29:46 +0100

    init

---
+++ configuration.nix
@@ -0,0 +1,133 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.useOSProber = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Ljubljana";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "sl_SI.UTF-8";
+    LC_IDENTIFICATION = "sl_SI.UTF-8";
+    LC_MEASUREMENT = "sl_SI.UTF-8";
+    LC_MONETARY = "sl_SI.UTF-8";
+    LC_NAME = "sl_SI.UTF-8";
+    LC_NUMERIC = "sl_SI.UTF-8";
+    LC_PAPER = "sl_SI.UTF-8";
+    LC_TELEPHONE = "sl_SI.UTF-8";
+    LC_TIME = "sl_SI.UTF-8";
+  };
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.tehnoplast = {
+    isNormalUser = true;
+    description = "tehnoplast";
+    extraGroups = [ "networkmanager" "wheel" "docker" ];
+    packages = with pkgs; [];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  wget
+  htop
+  git
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh = {
+    enable = true;
+
+    settings = {
+      PermitRootLogin = "no";
+      PasswordAuthentication = false;
+      PubkeyAuthentication = true;
+      ChallengeResponseAuthentication = false;
+      X11Forwarding = false;
+      UsePAM = true;
+      AllowUsers = [ "tehnoplast" ];
+    };
+  };
+
+  virtualisation.docker.enable = true;
+  # users.users.kristjan.extraGroups = [ "docker" ];
+  virtualisation.docker.rootless = {
+	  enable = true;
+	  setSocketVariable = true;
+  };
+
+  # Open ports in the firewall.
+  networking.firewall.allowedTCPPorts = [ 
+  8000 
+  8001 
+  # ngnix proxy manager
+  80
+  81 # gui
+  443
+  61892 # ngnix proy manager for ssh stream to forgejo
+  8080 # nextcloud (docker)
+  9051 # forgejo
+  222  # forgejo ssh
+  ];
+  # because of ngnix proxy manager allow ports below 1024
+  boot.kernel.sysctl = {
+    "net.ipv4.ip_unprivileged_port_start" = 80;
+  };
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.11"; # Did you read the comment?
+
+}
